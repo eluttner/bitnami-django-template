0f2da53335a13eda713e400080dbe73a375345ea
Eduardo Luttner <eluttner@gmail.com>
2015-01-27 14:25:40 -0200

    first commit

---
+++ rural_sustentavel/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for rural_sustentavel project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os, sys
+sys.path.append('/Applications/bitnami-djangostack-1.7-1/apps/django/django_projects/rural_sustentavel')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/Applications/bitnami-djangostack-1.7-1/apps/django/django_projects/rural_sustentavel/egg_cache")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rural_sustentavel.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
